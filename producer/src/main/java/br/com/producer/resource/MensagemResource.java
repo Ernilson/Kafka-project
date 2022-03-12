768c6d1eca619ad50444119ae8ddaaa5052316ed
Ernilson Daniel <ernilsondaniel@sisconsultoria.com.br>
2022-03-12 14:38:22 -0300

    primeiro kafka

---
+++ producer/src/main/java/br/com/producer/resource/MensagemResource.java
@@ -0,0 +1,25 @@
+package br.com.producer.resource;
+
+
+import br.com.producer.service.MensagemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mensagem")
+public class MensagemResource {
+
+    @Autowired
+    MensagemService mensagemService;
+
+    public MensagemResource() {
+        super();
+    }
+
+    @PostMapping
+    public ResponseEntity<String> enviarMensagem(@RequestBody String mensagem) {
+        mensagemService.sendMessage(mensagem);
+        return ResponseEntity.ok().body("Mensagem enviada com sucesso: " + mensagem);
+    }
+}
